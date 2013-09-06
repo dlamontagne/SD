9d8648842d02d60805f756fd9b4a4187962cd65e
Dustin <dslamontagne@ufl.edu>
2013-09-06 16:32:59 -0400

    collision detection and cursor follower
    1st big update

--- Assets/Scripts/charController.cs
+++ Assets/Scripts/charController.cs
@@ -6,23 +6,97 @@ public class charController : MonoBehaviour {
 	//Character Related
 	public float speed;
 	
+	private bool movedLeft;
+	private bool movedRight;
+	private bool movedUp;
+	private bool movedDown;
+	
+	private	RaycastHit hit;
+	bool contact;
+	
 	// Use this for initialization
 	void Start () {	
 		speed = 2f;
+		movedLeft = false;
+		movedRight = false;
+		movedUp = false;
+		movedDown = false;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () 
+	{
+		movedLeft = false;
+		movedRight = false;
+		movedUp = false;
+		movedDown = false;
 		
+
+		//Vector3 left1 = Vector3(-1.0f,0.0f,0.0f);
 		//Handle Input
-		if(Input.GetKey(KeyCode.A))
-			transform.Translate(-speed*Time.deltaTime,0f,0f);
-		if(Input.GetKey(KeyCode.D))
-			transform.Translate(speed*Time.deltaTime,0f,0f);
-		if(Input.GetKey(KeyCode.W))
-			transform.Translate(0f,0f,speed*Time.deltaTime);
-		if(Input.GetKey(KeyCode.S))
-			transform.Translate(0f,0f,-speed*Time.deltaTime);
-			
+		if(Input.GetKey(KeyCode.A))//left
+		{
+			contact = rigidbody.SweepTest(Vector3.left, out hit, speed*Time.deltaTime);
+			if(contact)
+			{
+				//print ("hiya");
+				transform.Translate((-hit.distance)+0.01f,0f,0f,Space.World);
+			}
+			else
+			{
+				transform.Translate(-speed*Time.deltaTime,0f,0f,Space.World);
+			}
+		}
+		if(Input.GetKey(KeyCode.D))//right
+		{
+			contact = rigidbody.SweepTest(Vector3.right, out hit, speed*Time.deltaTime);
+			if(contact)
+			{
+				//print ("hiya");
+				transform.Translate((hit.distance)-0.01f,0f,0f,Space.World);
+			}
+			else
+			{
+				transform.Translate(speed*Time.deltaTime,0f,0f,Space.World);
+			}	
+		}
+		if(Input.GetKey(KeyCode.W))//up
+		{
+			contact = rigidbody.SweepTest(Vector3.forward, out hit, speed*Time.deltaTime);
+			if(contact)
+			{
+				//print ("hiya");
+				transform.Translate(0f,0f,(hit.distance)-0.01f,Space.World);
+			}
+			else
+			{
+				transform.Translate(0f,0f,speed*Time.deltaTime,Space.World);
+			}	
+		}
+		if(Input.GetKey(KeyCode.S))//down
+		{
+			contact = rigidbody.SweepTest(Vector3.back, out hit, speed*Time.deltaTime);
+			if(contact)
+			{
+				//print ("hiya");
+				transform.Translate(0f,0f,(-hit.distance)+0.01f,Space.World);
+			}
+			else
+			{
+				transform.Translate(0f,0f,-speed*Time.deltaTime,Space.World);
+			}	
+		}
+		//	transform.Translate(0f,0f,-speed*Time.deltaTime);
+		
 	}
+	
+	/*void OnTriggerStay(Collider col) 
+	{
+		if(col.gameObject.tag== "Wall")
+		{ 
+			Vector3 pos = transform.position;
+			pos.z = col.transform.collider.bounds.min.z - transform.collider.bounds.extents.z;
+			transform.position = pos;				
+		}
+	}*/
 }
